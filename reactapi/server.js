e92c55daa5fec98366890895ca144d49544c0496
chongawin <chongawin@hotmail.com>
2021-05-11 17:50:47 +0700

    docker

---
+++ reactapi/server.js
@@ -0,0 +1,59 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data.json");
+const middlewares = jsonServer.defaults();
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "ExampleApi",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000/",
+      },
+    ],
+  },
+  apis: ["server.js"], // files containing annotations as above
+};
+
+const openapiSpecification = swaggerJsDoc(options);
+
+server.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiSpecification)); // Add custom routes before JSON Server router
+/**
+ * @swagger
+ * /data:
+ *  get:
+ *    description: Use to request all Example
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+server.get("/echo", (req, res) => {
+  res.jsonp(req.query);
+});
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares);
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+server.use(jsonServer.bodyParser);
+server.use((req, res, next) => {
+  if (req.method === "POST") {
+    req.body.createdAt = Date.now();
+  }
+  // Continue to JSON Server router
+  next();
+});
+
+// Use default router
+server.use(router);
+server.listen(8000, () => {
+  console.log("JSON Server is running");
+});
